732cf8b4eaa7b208d2a852100cf0c84c7bf24f8e
Matt Howard <github.severity519@passmail.net>
2025-06-15 08:11:55 -0400

    chore(ui): point entry fields at src & let Next transpile

---
+++ frontend/next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  transpilePackages: ['@iliad/stream-ui'],
+};
+
+export default nextConfig;
