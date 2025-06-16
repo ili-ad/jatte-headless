a7ec7a76dd3229ddc6ac90d817a3ee96d76d9701
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-15 23:14:45 -0400

    feat(adapter): implement getAppSettings

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -8,4 +8,5 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('api/app-settings/', views.get_app_settings, name='app-settings'),
 ]
