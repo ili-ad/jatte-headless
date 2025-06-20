bc1cb3f387cf1fada13b4e6874dc2058cbb0bff2
Matt Howard <github.severity519@passmail.net>
2025-06-19 20:41:10 -0400

    chore: Ensure backend & frontend deps installed; minimal smoke.

--- backend/jatte/urls.py
+++ backend/jatte/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('', include('accounts_supabase.urls')),
     path('', include('core.urls')),
     path('', include('chat.urls')),
     path('admin/', admin.site.urls),
+]
+
+#for dev credentials delete in prod:
+urlpatterns += [
+    path("api/token/", TokenObtainPairView.as_view(), name="token-obtain"),
 ]
