f71b6216091d770c93198f66dd27812b37a5a4b9
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-24 11:35:13 -0400

    feat(shim): add useMessageComposerHasSendableData placeholder

---
+++ libs/stream-chat-shim/src/useMessageComposerHasSendableData.ts
@@ -0,0 +1,11 @@
+/**
+ * Placeholder implementation of Stream's `useMessageComposerHasSendableData` hook.
+ *
+ * Always returns `false` until real message composer state is wired up.
+ */
+export const useMessageComposerHasSendableData = (): boolean => {
+  // TODO: connect to message composer state
+  return false;
+};
+
+export default useMessageComposerHasSendableData;
