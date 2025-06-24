507c8da85dfb2ea0fc2a3d8ffc0c96ecd0f44694
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-24 13:41:59 -0400

    Add shim for navigate-long-message-lists.stories

---
+++ libs/stream-chat-shim/src/navigate-long-message-lists.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+/** Placeholder for Storybook stories for navigating long message lists. */
+export default function NavigateLongMessageListsStories() {
+  return <div>navigate-long-message-lists.stories not implemented</div>;
+}
