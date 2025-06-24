13274dac73bff3bc3988386aa7b1fdc365ce3697
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-24 12:11:02 -0400

    chore(shim): add PollHeader placeholder

---
+++ libs/stream-chat-shim/src/PollHeader.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+
+/**
+ * Placeholder implementation of the PollHeader component.
+ * TODO: implement actual behavior matching stream-chat-react.
+ */
+export const PollHeader = () => {
+  return (
+    <div data-testid="poll-header-placeholder">PollHeader placeholder</div>
+  )
+}
+
+export default PollHeader
