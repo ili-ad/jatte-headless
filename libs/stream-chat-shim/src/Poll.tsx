c567bb59fcbaef69367935d66d7f4a2c252fbea0
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-24 11:47:10 -0400

    Add Poll component shim

---
+++ libs/stream-chat-shim/src/Poll.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import type { Poll as PollClass } from 'stream-chat';
+
+/**
+ * Placeholder implementation of the Stream Chat `Poll` component.
+ */
+export interface PollProps {
+  /** Poll data object from Stream Chat */
+  poll: PollClass;
+  /** Render a quoted poll if true */
+  isQuoted?: boolean;
+}
+
+export const Poll: React.FC<PollProps> = ({ isQuoted }) => {
+  return (
+    <div
+      data-testid={isQuoted ? 'quoted-poll-placeholder' : 'poll-placeholder'}
+    >
+      Poll placeholder
+    </div>
+  );
+};
+
+export default Poll;
