50fb177825a23a7d56b92d39815b699d22d6d9ed
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-24 11:50:51 -0400

    feat: add SuggestPollOptionForm shim

---
+++ libs/stream-chat-shim/src/SuggestPollOptionForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export type SuggestPollOptionFormProps = Record<string, any>;
+
+/**
+ * Placeholder implementation of SuggestPollOptionForm.
+ */
+export const SuggestPollOptionForm = (_props: SuggestPollOptionFormProps) => (
+  <div data-testid="suggest-poll-option-form-placeholder">
+    SuggestPollOptionForm
+  </div>
+);
+
+export default SuggestPollOptionForm;
