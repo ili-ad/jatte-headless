5cac3e87dce7c4ee014390a6e03ef7a3d56b1f79
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-24 13:40:56 -0400

    chore(shim): add placeholder for hello.stories

---
+++ libs/stream-chat-shim/src/hello.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+/** Placeholder for Storybook stories related to hello. */
+export default function HelloStories() {
+  return <div>hello.stories not implemented</div>;
+}
