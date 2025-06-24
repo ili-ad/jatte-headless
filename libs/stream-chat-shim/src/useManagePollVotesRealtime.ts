be7b139c3f8129bada3cf9c2bfed7305e898d5ba
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-24 12:15:09 -0400

    feat(shim): add useManagePollVotesRealtime

---
+++ libs/stream-chat-shim/src/useManagePollVotesRealtime.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import type { PollAnswer, PollVote } from 'stream-chat';
+import type { CursorPaginatorStateStore } from './useCursorPaginator';
+
+/**
+ * Placeholder for Stream\'s `useManagePollVotesRealtime` hook.
+ *
+ * Maintains a list of votes, but does not subscribe to real-time events yet.
+ */
+export function useManagePollVotesRealtime<T extends PollVote | PollAnswer = PollVote>(
+  _managedVoteType: 'answer' | 'vote',
+  cursorPaginatorState?: CursorPaginatorStateStore<T>,
+  _optionId?: string,
+) {
+  const [votesInRealtime, setVotesInRealtime] = useState<T[]>(
+    cursorPaginatorState?.getLatestValue().items ?? [],
+  );
+
+  useEffect(() => {
+    // TODO: connect to Stream Chat client and handle vote events
+    setVotesInRealtime(cursorPaginatorState?.getLatestValue().items ?? []);
+  }, [cursorPaginatorState]);
+
+  return votesInRealtime;
+}
