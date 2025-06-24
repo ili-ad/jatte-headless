b71852659fe16bdb821946611b3799cf09934dcb
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-24 09:15:47 -0400

    feat(shim): add useNotificationMessageNewListener

---
+++ libs/stream-chat-shim/src/useNotificationMessageNewListener.ts
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import type { Channel, Event } from 'stream-chat';
+
+/**
+ * Placeholder implementation of Stream's `useNotificationMessageNewListener` hook.
+ * It currently does not subscribe to any events.
+ */
+export const useNotificationMessageNewListener = (
+  setChannels: React.Dispatch<React.SetStateAction<Array<Channel>>>,
+  customHandler?: (
+    setChannels: React.Dispatch<React.SetStateAction<Array<Channel>>>,
+    event: Event,
+  ) => void,
+  allowNewMessagesFromUnfilteredChannels = true,
+) => {
+  useEffect(() => {
+    // TODO: wire up real Stream Chat client events
+  }, [setChannels, customHandler, allowNewMessagesFromUnfilteredChannels]);
+};
