bac09016a84c89c02fb042cc81eb90e733801430
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-24 10:19:25 -0400

    Add useMentionsHandler shim

---
+++ libs/stream-chat-shim/src/useMentionsHandler.ts
@@ -0,0 +1,39 @@
+import type { UserResponse } from 'stream-chat';
+
+export type CustomMentionHandler = (
+  event: React.BaseSyntheticEvent,
+  mentioned_users: UserResponse[],
+) => void;
+
+export type MentionedUserEventHandler = (
+  event: React.BaseSyntheticEvent,
+  mentionedUsers: UserResponse[],
+) => void;
+
+function createEventHandler(
+  fn?: CustomMentionHandler,
+  message?: { mentioned_users?: UserResponse[] },
+): MentionedUserEventHandler {
+  return (event) => {
+    if (typeof fn !== 'function' || !message?.mentioned_users?.length) {
+      return;
+    }
+    fn(event, message.mentioned_users);
+  };
+}
+
+export const useMentionsHandler = (
+  message?: { mentioned_users?: UserResponse[] },
+  customMentionHandler?: {
+    onMentionsClick?: CustomMentionHandler;
+    onMentionsHover?: CustomMentionHandler;
+  },
+) => {
+  const onMentionsClick = customMentionHandler?.onMentionsClick || (() => null);
+  const onMentionsHover = customMentionHandler?.onMentionsHover || (() => null);
+
+  return {
+    onMentionsClick: createEventHandler(onMentionsClick, message),
+    onMentionsHover: createEventHandler(onMentionsHover, message),
+  };
+};
