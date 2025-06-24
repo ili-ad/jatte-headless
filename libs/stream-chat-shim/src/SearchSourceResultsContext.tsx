72aedc5557262f98e4a438ce683c6dbc141203b6
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-24 12:40:59 -0400

    Add SearchSourceResultsContext shim

---
+++ libs/stream-chat-shim/src/SearchSourceResultsContext.tsx
@@ -0,0 +1,27 @@
+import type { PropsWithChildren } from 'react';
+import React, { createContext, useContext } from 'react';
+import type { SearchSource } from 'stream-chat';
+
+export type SearchSourceResultsContextValue = {
+  searchSource: SearchSource;
+};
+
+export const SearchSourceResultsContext = createContext<
+  SearchSourceResultsContextValue | undefined
+>(undefined);
+
+export const SearchSourceResultsContextProvider = ({
+  children,
+  value,
+}: PropsWithChildren<{
+  value: SearchSourceResultsContextValue;
+}>) => (
+  <SearchSourceResultsContext.Provider value={value as unknown as SearchSourceResultsContextValue}>
+    {children}
+  </SearchSourceResultsContext.Provider>
+);
+
+export const useSearchSourceResultsContext = () => {
+  const contextValue = useContext(SearchSourceResultsContext);
+  return contextValue as unknown as SearchSourceResultsContextValue;
+};
