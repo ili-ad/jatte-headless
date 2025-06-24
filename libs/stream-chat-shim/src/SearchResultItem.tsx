87586316ad7fb90252c622cf9a926f4b0742fff5
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-24 12:41:35 -0400

    Add SearchResultItem test shim

---
+++ libs/stream-chat-shim/src/SearchResultItem.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+
+/** Placeholder implementation of the SearchResultItem component. */
+export type SearchResultItemProps = Record<string, any>
+
+export const SearchResultItem = (_props: SearchResultItemProps) => {
+  return (
+    <div data-testid="search-result-item-placeholder">SearchResultItem placeholder</div>
+  )
+}
+
+export default SearchResultItem
