512eb4b7b16b1ecc8594f8240d609e4820d3db20
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-24 10:30:37 -0400

    feat: add SendToChannelCheckbox shim

---
+++ libs/stream-chat-shim/src/SendToChannelCheckbox.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/**
+ * Placeholder for Stream Chat's SendToChannelCheckbox component.
+ * TODO: implement real behaviour with message composer state.
+ */
+export const SendToChannelCheckbox = () => {
+  return (
+    <div data-testid="send-to-channel-checkbox-placeholder">
+      <input type="checkbox" />
+      <label>Send to channel</label>
+    </div>
+  )
+}
+
+export default SendToChannelCheckbox
