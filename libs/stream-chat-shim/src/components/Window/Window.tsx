03c4bf6d4695115f4d20e7cd0e2aebc3d5607cb1
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-26 15:26:32 -0400

    feat(shim): port Window component

---
+++ libs/stream-chat-shim/src/components/Window/Window.tsx
@@ -0,0 +1,30 @@
+import React, { PropsWithChildren } from 'react';
+import clsx from 'clsx';
+
+import { useChannelStateContext } from '../../context/ChannelStateContext';
+import type { LocalMessage } from 'stream-chat';
+
+export type WindowProps = {
+  /** optional prop to force addition of class str-chat__main-panel--thread-open to the Window root element */
+  thread?: LocalMessage;
+};
+
+/**
+ * A UI component for conditionally displaying a Thread or Channel
+ */
+const UnMemoizedWindow = (props: PropsWithChildren<WindowProps>) => {
+  const { children, thread: propThread } = props;
+  const { thread: contextThread } = useChannelStateContext('Window');
+
+  return (
+    <div
+      className={clsx('str-chat__main-panel', {
+        'str-chat__main-panel--thread-open': contextThread || propThread,
+      })}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const Window = React.memo(UnMemoizedWindow);
