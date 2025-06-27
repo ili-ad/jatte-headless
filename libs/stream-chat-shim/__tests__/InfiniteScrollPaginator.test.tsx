0e102556384257b1883452e610d6b9b2af960023
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-26 21:14:44 -0400

    feat(shim): port InfiniteScrollPaginator

---
+++ libs/stream-chat-shim/__tests__/InfiniteScrollPaginator.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { InfiniteScrollPaginator } from '../src/components/InfiniteScrollPaginator/InfiniteScrollPaginator';
+
+test('renders without crashing', () => {
+  render(<InfiniteScrollPaginator />);
+});
