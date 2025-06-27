b8191d5678857c15a859e89a6ac3640ee4c510bc
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-26 20:54:52 -0400

    feat(shim): add ChannelSearch SearchInput

---
+++ libs/stream-chat-shim/src/components/ChannelSearch/SearchInput.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+// import { useTranslationContext } from '../../context/TranslationContext'; // TODO backend-wire-up
+const useTranslationContext = (_componentName?: string) => ({
+  t: (key: string) => key,
+});
+
+export type SearchInputController = {
+  /** Clears the channel search state */
+  clearState: () => void;
+  inputRef: React.RefObject<HTMLInputElement | null>;
+  /** Search input change handler */
+  onSearch: React.ChangeEventHandler<HTMLInputElement>;
+  /** Current search string */
+  query: string;
+};
+
+export type AdditionalSearchInputProps = {
+  /** Sets the input element into disabled state */
+  disabled?: boolean;
+  /** Custom placeholder text to be displayed in the search input */
+  placeholder?: string;
+};
+
+export type SearchInputProps = AdditionalSearchInputProps & SearchInputController;
+
+export const SearchInput = (props: SearchInputProps) => {
+  const { disabled, inputRef, onSearch, placeholder, query } = props;
+
+  const { t } = useTranslationContext('SearchInput');
+
+  return (
+    <input
+      className='str-chat__channel-search-input'
+      data-testid='search-input'
+      disabled={disabled}
+      onChange={onSearch}
+      placeholder={placeholder ?? t('Search')}
+      ref={inputRef}
+      type='text'
+      value={query}
+    />
+  );
+};
