c0ab79cc3197cc578238280c7e52d43788bb50d0
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-26 22:24:49 -0400

    feat: add VirtualizedMessageList components

---
+++ libs/stream-chat-shim/__tests__/VirtualizedMessageList.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { VirtualizedMessageList } from '../src/components/MessageList/VirtualizedMessageList';
+
+test('renders without crashing', () => {
+  render(<VirtualizedMessageList />);
+});
