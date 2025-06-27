5f32f02debef2cc67cc56d9201457ebe245d2888
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-26 22:45:50 -0400

    feat(shim): port EndPollDialog

---
+++ libs/stream-chat-shim/src/components/Poll/PollActions/PollResults/index.ts
@@ -0,0 +1,1 @@
+// export * from './PollResults'; // TODO backend-wire-up
---
+++ libs/stream-chat-shim/src/components/Poll/PollActions/index.ts
@@ -0,0 +1,7 @@
+// export * from './AddCommentForm'; // TODO backend-wire-up
+export * from './EndPollDialog';
+// export * from './PollActions'; // TODO backend-wire-up
+// export * from './PollAnswerList'; // TODO backend-wire-up
+// export * from './PollOptionsFullList'; // TODO backend-wire-up
+// export * from './PollResults'; // TODO backend-wire-up
+// export * from './SuggestPollOptionForm'; // TODO backend-wire-up
