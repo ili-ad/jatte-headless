e4fc7f6213e371827567487ab25b6361ab9ca97e
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-26 22:54:35 -0400

    feat: port PollCreationDialog component

---
+++ libs/stream-chat-shim/src/components/Poll/PollCreationDialog/OptionFieldSet.tsx
@@ -0,0 +1,86 @@
+import clsx from 'clsx';
+import React, { useCallback } from 'react';
+import { FieldError } from '../../Form/FieldError';
+import { DragAndDropContainer } from '../../DragAndDrop/DragAndDropContainer';
+import { useTranslationContext } from '../../../context';
+import { useMessageComposer } from '../../MessageInput';
+import { useStateStore } from '../../../store';
+// import type { PollComposerState } from 'stream-chat'; // TODO backend-wire-up
+type PollComposerState = any;
+
+const pollComposerStateSelector = (state: PollComposerState) => ({
+  errors: state.errors.options,
+  options: state.data.options,
+});
+
+export const OptionFieldSet = () => {
+  const { pollComposer } = useMessageComposer();
+  const { errors, options } = useStateStore(
+    pollComposer.state,
+    pollComposerStateSelector,
+  );
+  const { t } = useTranslationContext('OptionFieldSet');
+
+  const onSetNewOrder = useCallback(
+    (newOrder: number[]) => {
+      const prevOptions = pollComposer.options;
+      pollComposer.updateFields({ options: newOrder.map((index) => prevOptions[index]) });
+    },
+    [pollComposer],
+  );
+
+  const draggable = options.length > 1;
+
+  return (
+    <fieldset className='str-chat__form__field str-chat__form__input-fieldset'>
+      <legend className='str-chat__form__field-label'>{t('Options')}</legend>
+      <DragAndDropContainer
+        className='str-chat__form__input-fieldset__values'
+        draggable={draggable}
+        onSetNewOrder={onSetNewOrder}
+      >
+        {options.map((option, i) => {
+          const error = errors?.[option.id];
+          return (
+            <div
+              className={clsx('str-chat__form__input-field', {
+                'str-chat__form__input-field--draggable': draggable,
+                'str-chat__form__input-field--has-error': error,
+              })}
+              key={`new-poll-option-${i}`}
+            >
+              <div className='str-chat__form__input-field__value'>
+                <FieldError
+                  className='str-chat__form__input-field__error'
+                  data-testid={'poll-option-input-field-error'}
+                  text={error && t(error)}
+                />
+                <input
+                  id={option.id}
+                  onBlur={() => {
+                    pollComposer.handleFieldBlur('options');
+                  }}
+                  onChange={(e) => {
+                    pollComposer.updateFields({
+                      options: { index: i, text: e.target.value },
+                    });
+                  }}
+                  onKeyUp={(event) => {
+                    if (event.key === 'Enter') {
+                      const nextInputId = options[i + 1].id;
+                      document.getElementById(nextInputId)?.focus();
+                    }
+                  }}
+                  placeholder={t('Add an option')}
+                  type='text'
+                  value={option.text}
+                />
+              </div>
+              {draggable && <div className='str-chat__drag-handle' />}
+            </div>
+          );
+        })}
+      </DragAndDropContainer>
+    </fieldset>
+  );
+};
---
+++ libs/stream-chat-shim/src/components/Poll/PollCreationDialog/PollCreationDialogControls.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useCanCreatePoll, useMessageComposer } from '../../MessageInput';
+import { useMessageInputContext, useTranslationContext } from '../../../context';
+
+export type PollCreationDialogControlsProps = {
+  close: () => void;
+};
+
+export const PollCreationDialogControls = ({
+  close,
+}: PollCreationDialogControlsProps) => {
+  const { t } = useTranslationContext('PollCreationDialogControls');
+  const { handleSubmit: handleSubmitMessage } = useMessageInputContext();
+  const messageComposer = useMessageComposer();
+  const canCreatePoll = useCanCreatePoll();
+
+  return (
+    <div className='str-chat__dialog__controls'>
+      <button
+        className='str-chat__dialog__controls-button str-chat__dialog__controls-button--cancel'
+        onClick={() => {
+          messageComposer.pollComposer.initState();
+          close();
+        }}
+      >
+        {t('Cancel')}
+      </button>
+      <button
+        className='str-chat__dialog__controls-button str-chat__dialog__controls-button--submit'
+        disabled={!canCreatePoll}
+        onClick={() => {
+          messageComposer
+            .createPoll()
+            .then(() => handleSubmitMessage())
+            .then(close)
+            .catch(console.error);
+        }}
+        type='submit'
+      >
+        {t('Create')}
+      </button>
+    </div>
+  );
+};
