39d9e9e67ccfbcd5d1e0e17da608a55d5f4204b7
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-26 22:10:40 -0400

    feat(chat-shim): add VirtualizedMessageList component

---
+++ libs/stream-chat-shim/src/components/MessageList/hooks/VirtualizedMessageList/useShouldForceScrollToBottom.ts
@@ -0,0 +1,36 @@
+import { useEffect, useRef } from 'react';
+import type { RenderedMessage } from '../../utils';
+import type { LocalMessage } from 'chat-shim';
+
+export function useShouldForceScrollToBottom(
+  messages: RenderedMessage[],
+  currentUserId?: string,
+) {
+  const lastFocusedOwnMessage = useRef('');
+  const initialFocusRegistered = useRef(false);
+
+  function recheckForNewOwnMessage() {
+    if (messages && messages.length > 0) {
+      const lastMessage = messages[messages.length - 1];
+
+      if (
+        (lastMessage as LocalMessage).user?.id === currentUserId &&
+        lastFocusedOwnMessage.current !== lastMessage.id
+      ) {
+        lastFocusedOwnMessage.current = lastMessage.id;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  useEffect(() => {
+    if (messages && messages.length && !initialFocusRegistered.current) {
+      initialFocusRegistered.current = true;
+      recheckForNewOwnMessage();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [messages, messages?.length]);
+
+  return recheckForNewOwnMessage;
+}
