61ae10727c26e4adefa637be68d21e4622c7a202
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-26 23:14:03 -0400

    feat: port ReactionsList component

---
+++ libs/stream-chat-shim/src/components/Reactions/index.ts
@@ -0,0 +1,7 @@
+export * from './ReactionsList';
+// TODO backend-wire-up: export * from './ReactionSelector';
+// TODO backend-wire-up: export * from './ReactionsListModal';
+// TODO backend-wire-up: export * from './SimpleReactionsList';
+// TODO backend-wire-up: export * from './SpriteImage';
+// TODO backend-wire-up: export * from './StreamEmoji';
+// TODO backend-wire-up: export * from './reactionOptions';
