a102deb6a2471468a94005a74efd358ffdaafe47
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-26 22:11:36 -0400

    feat(shim): add VirtualizedMessageList

---
+++ libs/stream-chat-shim/src/components/MessageList/hooks/useLastReadData.ts
@@ -0,0 +1,25 @@
+import { useMemo } from "react";
+
+import { isLocalMessage } from "../utils";
+import { getReadStates } from "../utils";
+
+import type { LocalMessage, UserResponse } from "stream-chat";
+import type { RenderedMessage } from "../utils";
+
+type UseLastReadDataParams = {
+  messages: RenderedMessage[];
+  returnAllReadData: boolean;
+  userID: string | undefined;
+  read?: Record<string, { last_read: Date; user: UserResponse }>;
+};
+
+export const useLastReadData = (props: UseLastReadDataParams) => {
+  const { messages, read, returnAllReadData, userID } = props;
+
+  return useMemo(() => {
+    const ownLocalMessages = messages.filter(
+      (msg) => isLocalMessage(msg) && msg.user?.id === userID,
+    ) as LocalMessage[];
+    return getReadStates(ownLocalMessages, read, returnAllReadData);
+  }, [messages, read, returnAllReadData, userID]);
+};
