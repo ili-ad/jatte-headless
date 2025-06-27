5bbd4f1e81a311c90458369adc6cf10b443a41e9
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-26 23:18:26 -0400

    feat(stream-chat-shim): add reactionOptions

---
+++ libs/stream-chat-shim/src/components/Reactions/reactionOptions.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable sort-keys */
+
+import React from 'react';
+
+// import { StreamEmoji } from './StreamEmoji'; // TODO backend-wire-up
+const StreamEmoji = (props: any) => null;
+
+export type ReactionOptions = Array<{
+  Component: React.ComponentType;
+  type: string;
+  name?: string;
+}>;
+
+export const defaultReactionOptions: ReactionOptions = [
+  {
+    type: 'haha',
+    Component: () => <StreamEmoji fallback='😂' type='haha' />,
+    name: 'Joy',
+  },
+  {
+    type: 'like',
+    Component: () => <StreamEmoji fallback='👍' type='like' />,
+    name: 'Thumbs up',
+  },
+  {
+    type: 'love',
+    Component: () => <StreamEmoji fallback='❤️' type='love' />,
+    name: 'Heart',
+  },
+  { type: 'sad', Component: () => <StreamEmoji fallback='😔' type='sad' />, name: 'Sad' },
+  {
+    type: 'wow',
+    Component: () => <StreamEmoji fallback='😲' type='wow' />,
+    name: 'Astonished',
+  },
+];
