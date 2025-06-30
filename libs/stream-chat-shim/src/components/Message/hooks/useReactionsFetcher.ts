1de447eaabe820aec11b36191035e1aa1e662afd
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-30 08:42:26 -0400

    feat(shim): implement queryReactions

--- libs/stream-chat-shim/src/components/Message/hooks/useReactionsFetcher.ts
+++ libs/stream-chat-shim/src/components/Message/hooks/useReactionsFetcher.ts
@@ -5,6 +5,7 @@ import type {
   ReactionSort,
   StreamChat,
 } from 'chat-shim';
+import { queryReactions } from '../../../chatSDKShim';
 import type { ReactionType } from '../../Reactions/types';
 
 export const MAX_MESSAGE_REACTIONS_TO_FETCH = 1000;
@@ -45,11 +46,15 @@ async function fetchMessageReactions(
   let hasNext = true;
 
   while (hasNext && reactions.length < MAX_MESSAGE_REACTIONS_TO_FETCH) {
-    const response = await Promise.resolve({
-      /* TODO backend-wire-up: queryReactions */
-      next: undefined,
-      reactions: [],
-    });
+    const response = await queryReactions(
+      { id: messageId } as any,
+      {
+        limit,
+        next,
+        reaction_type: reactionType,
+        sort,
+      },
+    );
 
     reactions.push(...response.reactions);
     next = response.next;
