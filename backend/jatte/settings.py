b35aa2490e9d556db942d433a39174b18d31e5c4
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-10-10 15:26:51 -0400

    feat(listRooms): expose rooms bootstrap endpoints

--- backend/jatte/settings.py
+++ backend/jatte/settings.py
@@ -50,7 +50,7 @@ if os.environ.get("PRINT_JWT_SECRET"):
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver"]
 
 AUTH_USER_MODEL = 'accounts_supabase.CustomUser'
 
@@ -91,6 +91,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts_supabase',
     'chat',
+    'rooms',
     'core',
 ]
 
