bf5ded4d999943f9207ea7d18294cf9085614cc5
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-12-10 19:48:04 -0500

    Normalize stream-chat-shim API and fix frontend imports

--- libs/stream-chat-shim/src/index.ts
+++ libs/stream-chat-shim/src/index.ts
@@ -1,6 +1,16 @@
 // make sure shim methods/state exist
 import 'chat-shim/polyfills';
 
+export { Chat, Channel, MessageInput, MessageList, TypingIndicator, Window } from './components';
+export { AIStateIndicator } from './components/AIStateIndicator';
+export { AIStates, useAIState } from './components/AIStateIndicator/hooks/useAIState';
+export { MessageSimple } from './components/Message/MessageSimple';
+export type { MessageProps } from './components/Message/types';
+export { StopAIGenerationButton } from './components/MessageInput/StopAIGenerationButton';
+export { WS_BASE } from './config/env';
+export { configureApiBase, setAuthToken } from './api/chatAPI';
+export { configureWebsocketBase } from './client';
+
 export * from './components';
 export * from './context';
 export * from './i18n';
